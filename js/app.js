2b88d5762bf70f48f900b40981ce2b3460cca6c1
unknown <cbjazz77@gmail.com>
2019-01-04 22:18:21 +0900

    first commit

---
+++ js/app.js
@@ -0,0 +1,154 @@
+/*
+*ChartGallary
+  ---  SerchBarForm
+  ---  ChartList
+       --- Chart
+*/
+class ChartGallary extends React.Component {
+
+  /* Query 결과의 StockList 정보를 가지고 있어서, ChartList 에 전달해야 함 ???*/
+  state = {
+    stocks: [],
+  }
+
+  /* 함수를 SearchBarForm 으로 전달해야 함 ???*/
+  loadStockChartFromServer = (searchQuery) => {
+    client.search(searchQuery, (serverCharts) => (
+      this.setState( {stocks: serverCharts})
+    ));
+  };
+
+  handleSearchSubmit = (searchQuery) => {
+    //this.loadStockChartFromServer(searchQuery);
+    alert(searchQuery)
+  };
+
+  render() {
+      return (
+        <div className='gallary'>
+          <div className='search'>
+            <SearchBarForm
+              onSearchSubmit={this.handleSearchSubmit}/>
+            <ChartList
+              stocks = {this.state.stocks}
+            />
+          </div>
+          <div className='contents'>
+          </div>
+        </div>
+      )
+  }
+}
+
+/*
+ChartGallary
+  ---  *SerchBarForm
+  ---  ChartList
+       --- Chart
+*/
+class SearchBarForm extends React.Component {
+  /* Search 에 대한 이벤트 핸들러 만들어야 함. (Enter 키 입력, search icon 클릭) */
+  state = {
+    searchQuery : '',
+  };
+
+  handleSearchQueryChange = (e) => {
+    this.setState( { searchQuery: e.target.value });
+  };
+
+  handleKeyPress = (e)  => {
+    if (e.key === 'Enter') {
+      this.props.onSearchSubmit(this.state.searchQuery);
+    }
+  };
+
+  handleSearchSubmit = () => {
+    this.props.onSearchSubmit(this.state.searchQuery);
+  };
+
+  render() {
+    return (
+      <div className='ui form'>
+        <div className='ui big icon fluid input focus'>
+          <i className="circular search link icon"
+            onClick={this.handleSearchSubmit}>
+          </i>
+          <input type='text'
+                className='input'
+                id='search_text'
+                placeholder="Search..."
+                onChange={this.handleSearchQueryChange}
+                onKeyPress={this.handleKeyPress} />
+        </div>
+      </div>
+    );
+  }
+}
+
+class ChartList extends React.Component {
+  /* Do not need constructor ...
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      products: [],
+    };
+
+    this.handleProductUpVote = this.handleProductUpVote.bind(this);
+  }
+  */
+  render() {
+
+    const stockComponents = this.props.stocks.map((stock) => (
+      // JSX component
+      //  key 는 React 의 유니크 바인딩을 위한 특별한 프로퍼티...
+      <Stock
+        key                 = {'stock-' + stock.id}
+        id                  = {stock.id}
+        title               = {stock.title}
+        description         = {stock.description}
+        chartImageUrl       = {stock.chartImageUrl}
+      />
+    ));
+
+    //"Product" 클래스에 대한 리스트를 리턴한다.
+    return (
+        <div className='ui unstackable items'>
+          {stockComponents}
+        </div>
+    );
+  }
+}
+
+// "props"는 부모 component 의 변수를 가져올 때 사용한다.
+class Stock extends React.Component {
+  /* Do not need constructor ...
+  constructor(props) {
+    super(props);
+
+    //binding this in funtion
+    this.handleUpVote = this.handleUpVote.bind(this);
+  }
+  */
+
+  render() {
+    return (
+      <div className='item'>
+        <div className='image'>
+          <img src={this.props.chartImageUrl} />
+        </div>
+        <div className='middle aligned content'>
+          <div className='description'>
+            <a href={this.props.url}>{this.props.title}</a>
+            <p>{this.props.description}</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <ChartGallary />,
+  document.getElementById('content')
+);
---
+++ js/seed.js
@@ -0,0 +1,35 @@
+
+  function generateVoteCount() {
+    return Math.floor((Math.random() * 50) + 15);
+  }
+
+  const data = [
+    {
+      id: 1,
+      title: '015260.KS',
+      description: 'A&P',
+      url: '#',
+      chartImageUrl: 'images/products/image-aqua.png',
+    },
+    {
+      id: 2,
+      title: '078520.KS',
+      description: 'ABLE C&C',
+      url: '#',
+      chartImageUrl: 'images/products/image-rose.png',
+    },
+    {
+      id: '3',
+      title: '095570.KS',
+      description: 'AJ Networks',
+      url: '#',
+      chartImageUrl: 'images/products/image-steel.png',
+    },
+    {
+      id: 4,
+      title: '001040.KS',
+      description: 'CJ',
+      url: '#',
+      chartImageUrl: 'images/products/image-yellow.png',
+    },
+  ];
